bf533654530fa3478bd7c87d76e951080b7f5e35
alexandercabanillas <acabanillas.playtechla@gmail.com>
2019-08-26 13:56:29 -0500

    Este es mi primer commit de proyecto heroes

---
+++ src/app/components/hereo/hereo.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router'
+import { HeroesService } from  '../../servicios/heroes.service';
+
+
+@Component({
+  selector: 'app-hereo',
+  templateUrl: './hereo.component.html',
+  styles: []
+})
+export class HereoComponent implements OnInit {
+
+  heroe:any = {}
+
+  constructor( private activatedRoute: ActivatedRoute,private _heroesService:HeroesService) {
+    this.activatedRoute.params.subscribe ( params =>{
+        this.heroe = this._heroesService.getHeroe(params['id']);
+    });
+   }
+
+  ngOnInit() {
+  }
+
+}
